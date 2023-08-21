917475f42a9fd7c2ba61cb9a628b859f859a285c
youmemonk <ukeym160@gmail.com>
2023-08-22 01:04:51 +0530

    another one

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,20 +1,5 @@
 import './App.css';
-// import Starter from './tutorial/01-useState/starter/01-error-example';
-// import Starter from './tutorial/01-useState/starter/02-useState-basics';
-// import Starter from './tutorial/01-useState/starter/03-useState-array';
-// import Starter from './tutorial/01-useState/starter/04-useState-object';
-// import Starter from './tutorial/01-useState/starter/05-useState-gotcha';
-// import Starter from './tutorial/02-useEffect/starter/04-fetch-data';
-// import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics';
-// import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
-// import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview';
-// import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples';
-// import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge';
-import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge';
-
-// import Final from './tutorial/01-useState/final/03-useState-array';
-// import Final from './tutorial/01-useState/final/04-useState-object';
-// import Final from './tutorial/01-useState/final/05-useState-gotcha';
+import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
 // import Final from './tutorial/02-useEffect/final/01-code-example';
 
 function App() {
--- advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+	const [toggle, setToggle] = useState(true);
+
+	const toggleValue = () => {
+		setToggle(!toggle);
+	};
+	return (
+		<>
+			<button className="btn" onClick={toggleValue}>
+				Toggle Component
+			</button>
+
+			{toggle && <RandomComponent />}
+		</>
+	);
+};
+
+const RandomComponent = () => {
+	useEffect(() => {
+		console.log('this is interesting');
+		const itemId = setInterval(() => {
+			console.log('hello form interval');
+		}, 1000);
+		return () => {
+			clearInterval(itemId);
+		};
+	});
+
+	return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
